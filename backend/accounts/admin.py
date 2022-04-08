802b527e6198e0cf019689f42658d8861b510dd1
amirSalmani <salmaniamir13@gmail.com>
2022-04-08 14:57:53 +0430

    add model user ad add to admin

---
+++ backend/accounts/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import User as CustomUser
+from .forms import UserCreationForm, UserChangeForm
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    form_add = UserCreationForm
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "is_admin",
+    )
+    list_filter = ("is_admin",)
+    search_fields = ("email", "username")
+    # list_editable
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "avatar",
+                    "password",
+                    "state",
+                    "city",
+                    "address",
+                    "plate",
+                    "zip_code",
+                    "wallet",
+                )
+            },
+        ),
+        ("Personal Info", {"fields": ("is_active",)}),
+        ("Personal Perms", {"fields": ("is_admin",)}),
+    )
+
+    add_fieldsets = ((None, {"fields": ("username", "password1", "password2")}),)
+
+    ordering = ("-id",)
+    filter_horizontal = ()
+
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.unregister(Group)
---
+++ backend/accounts/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.urls import reverse
+from django.utils.html import format_html
+from .managers import MyUserManager
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(max_length=150, blank=True)
+    first_name = models.CharField(max_length=150, null=True, blank=True, default="")
+    last_name = models.CharField(max_length=150, null=True, blank=True, default="")
+    avatar = models.ImageField(
+        upload_to="users/%Y/", null=True, blank=True, default=""
+    )
+    wallet = models.PositiveBigIntegerField(default=0)
+    # for address
+    state = models.CharField(max_length=200, null=True, blank=True, default="")
+    city = models.CharField(max_length=200, null=True, blank=True, default="")
+    address = models.TextField(null=True, blank=True, default="")
+    plate = models.CharField(
+        max_length=5, null=True, blank=True, default=""
+    )  # pelak khane
+    zip_code = models.CharField(max_length=10, null=True, blank=True, default="")
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+    objects = MyUserManager()
+
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_lable):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def get_absolute_url(self):
+        return reverse("accounts:user_update", args=[self.id])
+    
+    class Meta:
+        ordering = ('-id',)
