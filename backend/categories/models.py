df68f7a51c792818268b9938e63c8e148568a211
amirSalmani <salmaniamir13@gmail.com>
2022-04-09 15:25:41 +0430

    model added

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    name = models.CharField(max_length=240, unique=True)
+    slug = models.SlugField(max_length=250, allow_unicode=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = self.name.replace(' ', '-')
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
