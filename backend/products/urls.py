a68b0ebb9fe321d96efdf6860e78306e9db69a9a
amirSalmani <salmaniamir13@gmail.com>
2022-04-10 11:16:33 +0430

    add view product-by-category

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ListProducts.as_view(), name='products_list'),
+    path('products/<str:slug>/', views.ListProductsByCategory.as_view(), name='products_list_by_category'),
     path('products/create/', views.CreateProduct.as_view(), name='products_create'),
     path('products/rd/<int:pk>/', views.DetailDeleteProduct.as_view(), name='products_read_delete'),
     path('products/u/<int:pk>/', views.UpdateProduct.as_view(), name='products_update'),
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,9 +1,13 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import (
     ListAPIView,
     CreateAPIView,
     UpdateAPIView,
     RetrieveDestroyAPIView
 )
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from categories.models import Category
 from .models import Product
 from . import serializers
 
@@ -11,6 +15,14 @@ from . import serializers
 class ListProducts(ListAPIView):
     queryset = Product.objects.filter(hide=False)
     serializer_class = serializers.ProductsSerializerm1
+
+
+class ListProductsByCategory(APIView):
+    def get(self, request, slug):
+        category_obj = get_object_or_404(Category, slug=slug)
+        queryset = Product.objects.filter(categories=category_obj)
+        serializer = serializers.ProductsSerializerm1(queryset, many=True)
+        return Response(serializer.data, status=200)
 
 
 class CreateProduct(CreateAPIView):
