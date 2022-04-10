21b8483019a4d667f26d8771c2f63d0f2e23370b
amirSalmani <salmaniamir13@gmail.com>
2022-04-10 19:36:40 +0430

    fix import

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.urls import reverse
-from django.utils.html import format_html
 from .managers import MyUserManager
 
 
