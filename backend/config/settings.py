dbb6080d415471b5a14a5e3c668b4b226c63868b
amirSalmani <salmaniamir13@gmail.com>
2022-04-11 22:40:33 +0430

    add email config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -127,3 +127,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email settings
+DEFAULT_FROM_EMAIL = secret.or_email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = secret.or_email
+EMAIL_HOST_PASSWORD = secret.or_email_p
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True 
