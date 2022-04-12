638d7ed50fbce782c076699e485d5f8723deaec0
amirSalmani <salmaniamir13@gmail.com>
2022-04-13 00:20:10 +0430

    fix

--- backend/discount/cron.py
+++ backend/discount/cron.py
@@ -6,5 +6,5 @@ from datetime import date
 def my_scheduled_job():
     discounts = Discount.objects.all()
     for discount in discounts:
-        if discount.valid_to <= date.today():
+        if discount.valid_to < date.today():
             discount.delete()
