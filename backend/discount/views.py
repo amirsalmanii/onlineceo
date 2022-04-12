bfce589e668e7d07620f5e9c24fe527d30b54735
amirSalmani <salmaniamir13@gmail.com>
2022-04-12 14:24:12 +0430

    add view serializer url to model

--- backend/discount/views.py
+++ backend/discount/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+RetrieveUpdateAPIView,
+ListAPIView,
+CreateAPIView,
+RetrieveAPIView,
+DestroyAPIView
+)
+from .models import Discount
+from . import serializers
+# from product.views import MyPagination
+# YYYY-MM-DD
 
-# Create your views here.
+
+class DiscountListView(ListAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = serializers.DiscountListSerializer
+    # pagination_class = MyPagination
+
+
+class DiscountCreateView(CreateAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = serializers.DiscountCreateSerializer
+    # pagination_class = MyPagination
+
+
+class DiscountUpdateView(RetrieveUpdateAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = serializers.DiscountCreateSerializer
+
+
+class DiscountDetailView(RetrieveAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = serializers.DiscountListSerializer
+
+
+class DiscountDeletView(DestroyAPIView):
+    queryset = Discount.objects.all()
+    serializer_class = serializers.DiscountCreateSerializer
