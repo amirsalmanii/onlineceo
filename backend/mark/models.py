f60c45e361991cec8ffe46801c38949ff04bb860
amirSalmani <salmaniamir13@gmail.com>
2022-04-12 18:06:23 +0430

    add model

--- backend/mark/models.py
+++ backend/mark/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import User
+from products.models import Product
 
-# Create your models here.
+
+class Mark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='marks')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='marks')
+
+    def __str__(self):
+        return f'{str(self.user)} marked {self.product}'
+
+    class Meta:
+        ordering = ('-id',)
