7cf6b9fae18c83fd081759a2f3685c64e55a9603
amirSalmani <salmaniamir13@gmail.com>
2022-04-14 11:00:33 +0430

    clean

--- backend/discount/cron.py
+++ backend/discount/cron.py
@@ -3,6 +3,7 @@ from products.models import Product
 from .models import Discount
 from datetime import date
 
+
 def my_scheduled_job():
     discounts = Discount.objects.all()
     for discount in discounts:
