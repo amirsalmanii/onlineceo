ba95cd3bd0d48fc0027377054a8e710ee4aa5b8c
amirSalmani <salmaniamir13@gmail.com>
2022-04-19 17:12:54 +0430

    add new view

--- backend/categories/urls.py
+++ backend/categories/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('categories-m1/', views.ListCategories.as_view(), name='list_category'),
     path('categories-m2/', views.ListCategories2.as_view(), name='list_category2'),
+    path('categories-m3/', views.ListCategories3.as_view(), name='list_category3'),
     path('categories/<int:pk>/', views.DetailDeleteUpdateCategory.as_view(), name='rud_category'),
 ]
