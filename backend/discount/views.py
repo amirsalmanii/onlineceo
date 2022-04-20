d079f3238c2a478740bdc6efd49a4bd6adb6aeca
amirSalmani <salmaniamir13@gmail.com>
2022-04-20 20:23:46 +0430

    change discount system

--- backend/discount/views.py
+++ backend/discount/views.py
@@ -5,6 +5,8 @@ CreateAPIView,
 RetrieveAPIView,
 DestroyAPIView
 )
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Discount
 from . import serializers
 # from product.views import MyPagination
@@ -17,15 +19,37 @@ class DiscountListView(ListAPIView):
     # pagination_class = MyPagination
 
 
-class DiscountCreateView(CreateAPIView):
-    queryset = Discount.objects.all()
-    serializer_class = serializers.DiscountCreateSerializer
-    # pagination_class = MyPagination
+class DiscountCreateView(APIView):
+    def post(self, request):
+        serializer = serializers.DiscountCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            print('yes')
+            data = serializer.validated_data
+            products = data['products']
+            percent = data['discount_percent']
+            for pr in products:
+                pr.price_after_discount = pr.price - (1 / 100) * percent * pr.price
+                pr.save()
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
 
 
-class DiscountUpdateView(RetrieveUpdateAPIView):
-    queryset = Discount.objects.all()
-    serializer_class = serializers.DiscountCreateSerializer
+class DiscountUpdateView(APIView):
+    def put(self, request, pk):
+        print(request.data)
+        discount = Discount.objects.get(id=pk)
+        serializer = serializers.DiscountCreateSerializer(instance=discount, data=request.data)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            products = data['products']
+            percent = data['discount_percent']
+            for pr in products:
+                pr.price_after_discount = pr.price - (1 / 100) * percent * pr.price
+                pr.save()
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
 
 
 class DiscountDetailView(RetrieveAPIView):
