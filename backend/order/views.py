ae0281dcf6f886fdad1dab963a9c1c7e79932a25
amirSalmani <salmaniamir13@gmail.com>
2022-04-21 18:04:23 +0430

    fix many=true for serializer

--- backend/order/views.py
+++ backend/order/views.py
@@ -18,10 +18,10 @@ class CreateOrdersView(CreateAPIView):
 class RestriveOrdersView(APIView):
     def get(self, request, order_id):
         order = Order.objects.get(order_id=order_id)
-        order_items = OrderItems.objects.get(order_id=order_id)
+        order_items = OrderItems.objects.filter(order_id=order_id)
 
         serializer_order = serializers.OrderSerializerM1(order)
-        serializer_order_items = serializers.OrderItemsSerializerM1(order_items, context={'request': request})
+        serializer_order_items = serializers.OrderItemsSerializerM1(order_items, context={'request': request}, many=True)
         return Response({'data_1':serializer_order.data, 'data_2': serializer_order_items.data}, status=200)
     
     def put(self, request, order_id):
