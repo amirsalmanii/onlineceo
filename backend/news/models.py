3e32992f3902614de734bf0b34ebf4bf031a5f27
amirSalmani <salmaniamir13@gmail.com>
2022-04-24 17:04:40 +0430

    add model to app

--- backend/news/models.py
+++ backend/news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class News(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news')
+    title = models.CharField(max_length=240)
+    title_image = models.ImageField(upload_to='news/')
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
