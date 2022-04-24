a91f13ae5b70e592a68c5dfc274d9debde042877
amirSalmani <salmaniamir13@gmail.com>
2022-04-24 16:45:53 +0430

    add new url

--- backend/categories/urls.py
+++ backend/categories/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('categories-m1/', views.ListCategories.as_view(), name='list_category'),
     path('categories-m2/', views.ListCategories2.as_view(), name='list_category2'),
+    path('categories-m2-wp/', views.ListCategories2WithoutPagination.as_view(), name='list_category2-without-pagination'),
     path('categories-m3/', views.ListCategories3.as_view(), name='list_category3'),
     path('categories/<int:pk>/', views.DetailDeleteUpdateCategory.as_view(), name='rud_category'),
 ]
