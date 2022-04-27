17256d443f2150a85611d65d03d5c43993292b42
amirSalmani <salmaniamir13@gmail.com>
2022-04-27 19:00:38 +0430

    add pagination and search option

--- backend/news/views.py
+++ backend/news/views.py
@@ -1,13 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveDestroyAPIView, UpdateAPIView
 from rest_framework.response import Response
+from rest_framework import filters
 from .models import News
 from .serializers import NewsSerializer, NewsSerializer2
+from accounts.views import MyPagination
 
 
 class ListNewsView(ListAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+    pagination_class = MyPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author__first_name', 'body']
 
 
 class CreateNewsView(CreateAPIView):
