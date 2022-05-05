e7e168d25b13c4b13b65a144de47ee4f11287448
amirSalmani <salmaniamir13@gmail.com>
2022-05-05 23:43:46 +0430

    new serializers

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -18,8 +18,21 @@ class OrderSerializerM2(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class OrderSerializerM3(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        exclude = ('owner',)
+
+
 class OrderItemsSerializerM1(serializers.ModelSerializer):
     product = ProductsSerializerm2()
+    class Meta:
+        model = OrderItems
+        fields = '__all__'
+
+
+class OrdersItemCreateSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = OrderItems
         fields = '__all__'
