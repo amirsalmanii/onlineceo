010e514e32708c833407acca75af92a3ad56f9f6
amirsalmanii <salmaniamir13@gmail.com>
2022-05-29 13:00:43 +0100

    updated

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,7 +140,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
